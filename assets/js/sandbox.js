9fc3c40cb8aaf3ec4169a3f44dc5975c97a45ec0
Tayomide <tibrahim@hawk.iit.edu>
2022-02-12 17:42:17 -0600

    Populate individual lists with tags

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -39,12 +39,21 @@ var default_selector = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 function makeOL(array, id, selected){
   var i;
+  var listItems;
 
   // Add appropriate number of list
   for ( i = 0; i < selected.length; i++) {
     document.getElementById(id).appendChild(document.createElement('li'));
   }
-  
+
+  listItems = document.getElementById(id).getElementsByTagName('li');
+
+  for ( i in selected ) {
+    listItems[i].appendChild(document.createElement('img'));
+    listItems[i].appendChild(document.createElement('p'));
+    listItems[i].appendChild(document.createElement('p'));
+    listItems[i].appendChild(document.createElement('p'));
+  }
 }
 
 // Home page initialization
